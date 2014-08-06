2c0c8e24cc4e7acaa4f337ef22b239d16b01f0d7
Paul Bill <paul@develodesign.co.uk>
2014-08-06 09:25:52 +0100

    Adding radio buttons support

--- scripts/develo-pretty-form-controls.js
+++ scripts/develo-pretty-form-controls.js
@@ -1,10 +1,19 @@
 /**
  * Created by paul on 05/08/2014.
  */
+
 ( function( $ ){
 	"use strict";
 
-	var BasePrettyFormControl = function( $el, options ){
+	/**
+	 * Pretty Form checkboxes
+	 *
+	 * @param $el
+	 * @param options
+	 *
+	 * @constructor
+	 */
+	var PrettyFormCheckbox = function( $el, options ){
 
 		this.options = options;
 		this.$el = $el;
@@ -12,65 +21,149 @@
 		this.initialize();
 	};
 
-	BasePrettyFormControl.prototype.initialize = function(){
-
-		this.setupBindings();
-	};
-
-	BasePrettyFormControl.prototype.setupBindings = function(){
-		// Overridden by child
-	};
-
-	var PrettyFormCheckbox = BasePrettyFormControl.extend();
-
+	/**
+	 * Initialise the plugin, cache reference to input and checked value
+	 */
 	PrettyFormCheckbox.prototype.initialize = function(){
 
 		this.$input = this.$el.find( 'input' );
 		this.isChecked = this.$input.is( ':checked' );
 
-		BasePrettyFormControl.prototype.initialize.apply( this, arguments );
+		this.setupBindings();
 	};
 
+	/**
+	 * Setup bindings for clicking the element
+	 */
 	PrettyFormCheckbox.prototype.setupBindings = function(){
 
-		var thisCheckbox = this;
-		this.$el.on( 'click', thisCheckbox.onClick );
+		this.$el.on( 'click', $.proxy( this.onClick, this ) );
 	};
 
+	/**
+	 * Called when the element has been clicked, prevent default action and toggle the input element
+	 *
+	 * @param event
+	 */
 	PrettyFormCheckbox.prototype.onClick = function( event ){
 
+		event.preventDefault();
 		this.toggleChecked();
 	};
 
+	/**
+	 * Toggles the input element.
+	 */
 	PrettyFormCheckbox.prototype.toggleChecked = function(){
 
-		this.isChecked ?
-			this.unCheck():
+		if( this.isChecked )
+			this.unCheck();
+
+		else
 			this.check();
 	};
 
-	PrettyFormCheckbox.check = function(){
+	/**
+	 * Checks the input element and adds active class to the element
+	 */
+	PrettyFormCheckbox.prototype.check = function(){
 
 		this.isChecked = true;
 		this.$input.prop( 'checked', true );
 		this.$el.addClass( this.options.activeClass );
 	};
 
-	PrettyFormCheckbox.unCheck = function(){
+	/**
+	 * Unchecks the input element and removes active class to the element
+	 */
+	PrettyFormCheckbox.prototype.unCheck = function(){
 
 		this.isChecked = false;
 		this.$input.prop( 'checked', false );
 		this.$el.removeClass( this.options.activeClass );
 	};
 
-	var checkBoxDefaultOptions = {
+	/**
+	 * Radios buttons
+	 *
+	 * @extends PrettyFormCheckbox
+	 *
+	 * @param $el
+	 * @param options
+	 *
+	 * @constructor
+	 */
+	var PrettyFormRadios = function( $el, options ){
+
+		PrettyFormCheckbox.apply( this, arguments );
+	};
+	PrettyFormRadios.prototype = Object.create( PrettyFormCheckbox.prototype );
+	PrettyFormRadios.prototype.constructor = PrettyFormRadios;
+
+	/**
+	 * Override the parent on click to turn off all radios before calling parent method.
+	 * @param event
+	 */
+	PrettyFormRadios.prototype.onClick = function( event ){
+
+		this.turnOffAllRadios();
+
+		PrettyFormCheckbox.prototype.onClick.apply( this, arguments );
+	};
+
+	/**
+	 * Finds all the radios siblings.
+	 *
+	 * @returns {*}
+	 */
+	PrettyFormRadios.prototype.findSiblings = function(){
+
+		var radioGroup = this.$input.prop( 'name' );
+		var elementClass = this.$el.prop( 'class' ).replace( this.options.activeClass );
+		var $siblings = $( 'input[name=' + radioGroup + ']' ).parents( this.options.siblingParentSelector ).find( '.' + elementClass );
+
+		return $siblings;
+	};
+
+	/**
+	 * Turn off all the radios in the collection.
+	 */
+	PrettyFormRadios.prototype.turnOffAllRadios = function(){
+
+		var $siblings = this.findSiblings();
+
+		$siblings.each( $.proxy( function( index, siblingEl ){
+
+			var $sibling = $( siblingEl );
+			var formControl = $sibling.data( 'prettyFormControl' );
+
+			if( formControl != this ) {
+				formControl.unCheck();
+			}
+
+		}, this ) );
+	};
+
+	/**
+	 * Default options
+	 */
+	var	checkBoxDefaultOptions = {
 		activeClass: 'checked'
 	};
+
+	var radioDefaultOptions = $.extend( true , {}, checkBoxDefaultOptions, {
+		siblingParentSelector: '.pretty-forms.radios'
+	} );
 
 	var defaultOptions = {
 		type: 'checkboxes'
 	};
 
+	/**
+	 * Initialise the plugin
+	 *
+	 * @param options
+	 */
 	$.fn.prettyFormControl = function( options ){
 
 		options = $.extend( true, defaultOptions, options );
@@ -83,8 +176,16 @@
 			case 'checkbox':
 			case 'checkboxes':
 
-				options = $.extend( true, checkBoxDefaultOptions, options );
+				options = $.extend( true, {}, checkBoxDefaultOptions, options );
 				ClassToLoad = PrettyFormCheckbox;
+
+				break;
+
+			case 'radio':
+			case 'radios':
+
+				options = $.extend( true, {}, radioDefaultOptions, options );
+				ClassToLoad = PrettyFormRadios;
 
 				break;
 		}
@@ -93,7 +194,8 @@
 
 			var $el = $( this );
 
-			$el.data( 'prettyFormControl', new ClassToLoad( $el, options ) );
+			if( ! $el.data( 'prettyFormControl' ) )
+				$el.data( 'prettyFormControl', new ClassToLoad( $el, options ) );
 		});
 
 	};
